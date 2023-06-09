aeebb23c4453101e75a02012468eb16004b8c45e
Eric Vo <tvo12@ewu.edu>
2023-06-08 17:17:52 -0700

    cookies wiper

--- PHP/logout.php
+++ PHP/logout.php
@@ -1,13 +1,26 @@
 <?php
 // Initialize the session
 session_start();
- 
+
 // Unset all of the session variables
 $_SESSION = array();
- 
+
 // Destroy the session.
 session_destroy();
- 
+
+// Delete regular HTTP cookies
+$cookies = $_COOKIE;
+foreach ($cookies as $cookie_name => $cookie_value) {
+    setcookie($cookie_name, '', time() - 3600, '/');
+}
+
+// Delete jQuery cookies
+echo '<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>';
+echo '<script>';
+echo '$.removeCookie("email", { path: "/" });';
+echo '$.removeCookie("password", { path: "/" });';
+echo '</script>';
+
 // Redirect to login page
 header("location: ../HTML/index.html");
 exit;
